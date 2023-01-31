c58f3b75926acba741a22d5d577c42a167b5fe08
Bruno <bruno_moises_stz@hotmail.com>
2023-01-30 21:22:53 -0300

    Finalizando curso - Laravel: criando uma aplicação com MVC

--- Laravel/controle-series/app/Http/Controllers/SeriesController.php
+++ Laravel/controle-series/app/Http/Controllers/SeriesController.php
@@ -2,24 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-        $series = [
-            'Punisher',
-            'Lost',
-            'Grey\'s Anatomy'
-        ];
+        $series = Serie::all();
 
-        $html = '<ul>';
-        foreach($series as $serie) {
-            $html .= "<li>$serie</li>";
-        }
-        $html .= '</ul>';
+        return view('series.index', compact('series'));
+    }
 
-        return $html;
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nomeSerie = $request->input('nome');
+        $serie = new Serie();
+        $serie->nome = $nomeSerie;
+        $serie->save();
+        return redirect('/series');
     }
 }
