e75faddabc973fbfa69acd87a77d49af83d4ff62
Bruno <bruno_moises_stz@hotmail.com>
2023-03-04 17:04:24 -0300

    Finalizando curso - Go e Gin: criando API rest com simplicidade

---
+++ GoLang/gin-api-rest/routes/routes.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"gin-api-rest/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HandleRequests() {
+	r := gin.Default()
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	r.GET("/:nome", controllers.Saudacao)
+	r.GET("/alunos/:id", controllers.BuscaAluno)
+	r.POST("/alunos", controllers.CriaNovoAluno)
+	r.DELETE("/alunos/:id", controllers.DeletaAluno)
+	r.PATCH("/alunos/:id", controllers.EditaAluno)
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaPorCPF)
+	r.Run()
+}
