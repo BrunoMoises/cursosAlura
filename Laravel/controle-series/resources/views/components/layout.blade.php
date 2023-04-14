d4d97ebc56c24531a401146f9ed6a27932af215e
Bruno <bruno_moises_stz@hotmail.com>
2023-04-14 10:47:21 -0300

    Finalizar curso - Laravel: transações, service container e autenticação

--- Laravel/controle-series/resources/views/components/layout.blade.php
+++ Laravel/controle-series/resources/views/components/layout.blade.php
@@ -5,11 +5,33 @@
     <meta charset="UTF-8">
     <title>{{ $title }}</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
 </head>
 
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="{{ route('series.index') }}">Home</a>
+
+        @auth
+            <form action="{{ route('logout') }}" method="post">
+                @csrf
+                <button class="btn btn-link">Sair</button>
+            </form>
+        @endauth
+
+        @guest
+        <a href="{{ route('login') }}">Entrar</a>
+        @endguest
+</nav>
     <div class="container">
         <h1>{{ $title }}</h1>
+
+        @isset($mensagemSucesso)
+            <div class="alert alert-success">
+                {{ $mensagemSucesso }}
+            </div>
+        @endisset
 
         @if ($errors->any())
             <div class="alert alert-danger">
--- Laravel/controle-series/resources/views/series/index.blade.php
+++ Laravel/controle-series/resources/views/series/index.blade.php
@@ -1,18 +1,15 @@
-<x-layout title="Series">
+<x-layout title="Series" :mensagem-sucesso="$mensagemSucesso">
+    @auth
     <a href="{{ route('series.create') }}" class="btn btn-dark mb-2">Adicionar</a>
-
-    @isset($mensagemSucesso)
-        <div class="alert alert-success">
-            {{ $mensagemSucesso }}
-        </div>
-    @endisset
+    @endauth
 
     <ul class="list-group">
         @foreach ($series as $serie)
             <li class="list-group-item d-flex justify-content-between align-items-center btn-sn">
-                <a href="{{ route('seasons.index', $serie->id) }}">
+                @auth<a href="{{ route('seasons.index', $serie->id) }}">@endauth
                     {{ $serie->nome }}
-                </a>
+                    @auth</a>
+
                 <span class="d-flex">
                     <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary">
                         E
@@ -25,6 +22,7 @@
                         </button>
                     </form>
                 </span>
+                @endauth
             </li>
         @endforeach
     </ul>
